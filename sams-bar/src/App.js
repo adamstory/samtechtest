2e7614d560af77a1a9d06f340e4ec1271ee8db72
adamstory <adamjstory@gmail.com>
2022-03-17 16:38:52 +0000

    Create variables to store required data from fetch

--- sams-bar/src/App.js
+++ sams-bar/src/App.js
@@ -3,12 +3,20 @@ import React, {useEffect} from "react";
 
 function App() {
 
+// Async function to fetch from API
 async function getCocktailData() {
   const response = await fetch ("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=Margarita")
   const data = await response.json();
   console.log(data);
+// Cocktail details
+  let cocktailName = data.drinks[0].strDrink;
+  let cocktailImageURL = data.drinks[0].strImageSource;
+  let cocktailIngredientOne = data.drinks[0].strIngredient1;
+  let cocktailIngredientTwo = data.drinks[0].strIngredient2;
+  console.log(cocktailName, cocktailImageURL, cocktailIngredientOne, cocktailIngredientTwo);
 }
 
+// Use Effect hook to manage effects of HTTP req after render
 useEffect(()=>{
   getCocktailData();
 })
