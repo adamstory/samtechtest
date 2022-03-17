23d0787f7c8c283d7342b75a5ee4c043ac6a94e6
adamstory <adamjstory@gmail.com>
2022-03-17 17:12:35 +0000

    Fetch cocktails from search input and display on page

--- sams-bar/src/App.js
+++ sams-bar/src/App.js
@@ -1,37 +1,50 @@
 import "./App.css";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [cocktailName, setCocktailName] = useState("");
+  const [cocktailImage, setCocktailImage] = useState("");
+  const [cocktailIngredientOne, setCocktailIngredientOne] = useState("");
+  const [cocktailIngredientTwo, setCocktailIngredientTwo] = useState("");
+
   // Async function to fetch from API
   async function getCocktailData(searchTerm) {
     const response = await fetch(
       `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
     );
     const data = await response.json();
-    console.log(data);
-    // Cocktail details
-    let cocktailName = data.drinks[0].strDrink;
-    let cocktailImageURL = data.drinks[0].strImageSource;
-    let cocktailIngredientOne = data.drinks[0].strIngredient1;
-    let cocktailIngredientTwo = data.drinks[0].strIngredient2;
-    console.log(
-      cocktailName,
-      cocktailImageURL,
-      cocktailIngredientOne,
-      cocktailIngredientTwo
-    );
+
+    // Set cocktail details
+    setCocktailName(data.drinks[0].strDrink);
+    setCocktailImage(data.drinks[0].strDrinkThumb);
+    setCocktailIngredientOne(data.drinks[0].strIngredient1);
+    setCocktailIngredientTwo(data.drinks[0].strIngredient2);
   }
 
-  // Use Effect hook to manage effects of HTTP req after render
+  // Use Effect hook to manage effects of HTTP req after render, only search when search is changed
   useEffect(() => {
-    getCocktailData("Margarita");
-    getCocktailData("Old fashioned");
-    getCocktailData("Negroni");
-  });
+    getCocktailData(searchTerm);
+  }, [searchTerm]);
 
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">
+        <h1>{`Welcome to Sam's Bar!`}</h1>
+        <h2>Enter a name of a cocktail in the input field</h2>
+        <input
+          type="text"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        ></input>
+        <div className="cocktail_info">
+          <h2 className="cocktail_name">Cocktail Name: {cocktailName}</h2>
+          <img src={cocktailImage} alt="cocktail_image"></img>
+          <h3 className="cocktail_ingredients">
+            Ingredients: {cocktailIngredientOne}, {cocktailIngredientTwo}
+          </h3>
+        </div>
+      </header>
     </div>
   );
 }
